cac4b2c81e4ca7e02140a607437e386c5d10dcf6
imkr099 <imkr099@gmail.com>
2022-12-19 19:27:26 +0600

    hw3

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from aiogram.utils import executor
-from handlers import client, callback, extra, admin
+from handlers import client, callback, extra, admin, fsm_mentor
 from config import dp
 import logging
 
 client.register_handlers_client(dp)
 callback.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
+fsm_mentor.register_handlers_fsm_mentor(dp)
 
 extra.register_handler_extra(dp)
 

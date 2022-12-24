c5dde5765712eb6a850316da403ca18d8d4ea35d
imkr099 <imkr099@gmail.com>
2022-12-24 21:44:26 +0600

    hw4

--- database/bot_db.py
+++ database/bot_db.py
@@ -1,26 +1,26 @@
 import random
 import sqlite3
 
+
 def sql_create():
     global db, cursor
     db = sqlite3.connect("bot.sqlite3")
     cursor = db.cursor()
 
-
     if db:
         print("База данных подключена")
 
     db.execute("CREATE TABLE IF NOT EXISTS sultan"
-               "(id INTEGER PRIMARY KEY, username TEXT, "
+               "(id INTEGER PRIMARY KEY,"
                "name TEXT, course TEXT, "
-               "age INTEGER, group INTEGER)")
+               "age INTEGER, groupp INTEGER)")
     db.commit()
 
 
 async def sql_command_insert(state):
     async with state.proxy() as data:
         cursor.execute("INSERT INTO sultan VALUES "
-                       "(?, ?, ?, ?, ?, ?)", tuple(data.values()))
+                       "(?, ?, ?, ?, ?)", tuple(data.values()))
         db.commit()
 
 
@@ -28,9 +28,7 @@ async def sql_command_random(message):
     result = cursor.execute("SELECT * FROM sultan").fetchall()
     random_user = random.choice(result)
     await message.answer(
-        random_user[6],
-        caption=f"{random_user[1]} {random_user[2]} {random_user[3]}"
-                f"\n{random_user[4]} {random_user[5]} {random_user[0]}"
+        f"{random_user[1]} {random_user[2]} {random_user[3]} {random_user[4]}"
     )
 
 
@@ -38,6 +36,6 @@ async def sql_command_all():
     return cursor.execute("SELECT * FROM sultan").fetchall()
 
 
-async def sql_command_delete():
-    cursor.execute("DELETE FROM sultan WHERE id = ?", (user_id))
+async def sql_command_delete(user_id):
+    cursor.execute("DELETE FROM sultan WHERE id = ?", (user_id,))
     db.commit()
--- handlers/admin.py
+++ handlers/admin.py
@@ -2,7 +2,8 @@ from aiogram import types, Dispatcher
 from config import bot
 from config import ADMINS
 from database.bot_db import sql_command_all, sql_command_delete
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
 async def ban(message: types.Message):
     if message.chat.type != "private":
         if message.from_user.id not in ADMINS:
@@ -24,19 +25,15 @@ async def delete_data(message: types.Message):
     else:
         users = await sql_command_all()
         for user in users:
-            await message.answer_photo(
-                user[6],
-                caption=f"{user[2]} {user[3]} {user[4]} "
-                        f"{user[5]}\n{user[1]}",
-                reply_markup=InlineKeyboardMarkup().add(
-                    InlineKeyboardButton(f"delete {user[2]}",
-                                         callback_data=f"delete {user[0]}")
-                )
+            await message.reply(
+                user[1],
+                f"{user[2]} {user[3]} {user[4]} "
+                f"\n{user[1]}"
             )
 
 
 async def complete_delete(call: types.CallbackQuery):
-    await sql_command_delete(call.data.replace("delete ", ""))
+    await sql_command_delete(call.data.startswith("delete "))
     await call.answer(text="Удалено", show_alert=True)
     await bot.delete_message(call.from_user.id, call.message.message_id)
 
